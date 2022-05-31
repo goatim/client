dc17b0241f88b3a166b6771b11f4c62fadad569e
Lucien Perouze <lucien.perouze@cezemb.re>
2022-05-31 13:38:25 +0200

    Dividend asset type

--- src/models/dividend.ts
+++ src/models/dividend.ts
@@ -3,7 +3,7 @@ import Asset from './asset';
 import PhysicalEvent from './physicalEvent';
 
 export default interface Dividend extends Model {
-  asset?: Asset;
+  asset?: Asset | string;
   physical_event?: PhysicalEvent | string;
   percentage?: number;
   amount?: number;
