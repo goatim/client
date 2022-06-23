40f7bbf659d76731692547f2d202c579afe68f48
Lucien Perouze <lucien.perouze@cezemb.re>
2022-06-23 18:03:58 +0200

    Move notifications events

--- src/notifications/model.ts
+++ src/notifications/model.ts
@@ -1,12 +1,11 @@
 import { Model } from '../api';
 import User from '../auth/users/model';
 import Wallet from '../market/wallets/model';
-import NotificationEvent from './events';
 
 export default interface Notification<P = unknown> extends Model {
   user?: User | string;
   wallet?: Wallet | string;
-  event?: NotificationEvent;
+  event?: string;
   payload?: P;
   seen_at?: string;
   read_at?: string;
