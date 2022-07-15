cdcd12ae4b585236d688d3f2afd5621d3a95592c
Lucien Perouze <lucien.perouze@cezemb.re>
2022-07-15 15:15:14 +0200

    Spotlight

---
+++ src/soccer/spotlights/api.ts
@@ -0,0 +1,117 @@
+import {
+  useMutation,
+  UseMutationResult,
+  useQuery,
+  useQueryClient,
+  UseQueryResult,
+} from 'react-query';
+import { PaginatedList, RequestBody, RequestParams, useApi } from '../../api';
+import Spotlight from './model';
+
+export function useSpotlight(id?: string): UseQueryResult<Spotlight> {
+  const api = useApi();
+  return useQuery<Spotlight>(
+    ['spotlights', id],
+    async () => {
+      const { data } = await api.get<Spotlight>(`/spotlights/${id}`);
+      return data;
+    },
+    {
+      enabled: !!id,
+    },
+  );
+}
+
+export type SpotlightList = PaginatedList<'spotlights', Spotlight>;
+
+export interface GetSpotlightsParams extends RequestParams {
+  tags?: string[] | string;
+  club?: string;
+  league?: string;
+}
+
+export function useSpotlights(params?: GetSpotlightsParams): UseQueryResult<SpotlightList> {
+  const api = useApi();
+  return useQuery<SpotlightList>(['spotlights', params], async () => {
+    const { data } = await api.get<SpotlightList>('/spotlights', params);
+    return data;
+  });
+}
+
+export interface SpotlightBody extends RequestBody {
+  tags?: string;
+  type?: string;
+  subtitle?: string;
+  title?: string;
+  description?: string;
+  primary_color?: string;
+  secondary_color?: string;
+  club?: string;
+  league?: string;
+  primary_assets?: string;
+  secondary_assets?: string;
+}
+
+export function useCreateSpotlight(): UseMutationResult<Spotlight, unknown, SpotlightBody> {
+  const api = useApi();
+  const queryClient = useQueryClient();
+  return useMutation<Spotlight, unknown, SpotlightBody>(
+    async (body: SpotlightBody) => {
+      const { data } = await api.post<Spotlight>('/spotlights', body);
+      return data;
+    },
+    {
+      onSuccess(spotlight: Spotlight) {
+        queryClient.setQueryData(['spotlights', spotlight.id], spotlight);
+      },
+    },
+  );
+}
+
+export type UpdateSpotlightVariables = SpotlightBody & { id: string };
+
+export function useUpdateSpotlight(): UseMutationResult<
+  Spotlight,
+  unknown,
+  UpdateSpotlightVariables
+> {
+  const api = useApi();
+  const queryClient = useQueryClient();
+  return useMutation<Spotlight, unknown, UpdateSpotlightVariables>(
+    async ({ id, ...body }: UpdateSpotlightVariables) => {
+      const { data } = await api.put<Spotlight>(`/spotlights/${body}`, body);
+      return data;
+    },
+    {
+      onSuccess(spotlight: Spotlight) {
+        queryClient.setQueryData(['spotlights', spotlight.id], spotlight);
+      },
+    },
+  );
+}
+
+export type AddSpotlightPictureBody = { illustration: File };
+
+export type AddSpotlightPictureVariables = AddSpotlightPictureBody & { id: string };
+
+export function useAddSpotlightIllustration(): UseMutationResult<
+  Spotlight,
+  unknown,
+  AddSpotlightPictureVariables
+> {
+  const api = useApi();
+  const queryClient = useQueryClient();
+  return useMutation<Spotlight, unknown, AddSpotlightPictureVariables>(
+    async ({ id, illustration }: AddSpotlightPictureVariables) => {
+      const { data } = await api.post<Spotlight>(`/spotlights/${id}/illustration`, {
+        illustration,
+      });
+      return data;
+    },
+    {
+      onSuccess(spotlight: Spotlight) {
+        queryClient.setQueryData(['spotlights', spotlight.id], spotlight);
+      },
+    },
+  );
+}
---
+++ src/soccer/spotlights/model.ts
@@ -0,0 +1,27 @@
+import League from '../leagues/model';
+import Club from '../clubs/model';
+import Asset from '../../trading/assets/model';
+import { Image } from '../../medias/image';
+import { Model } from '../../api';
+
+export type SpotlightType = 'simple' | 'duo';
+
+export default interface Spotlight extends Model {
+  tags?: string[];
+  type?: SpotlightType;
+  subtitle?: string;
+  title?: string;
+  description?: string;
+  primary_color?: string;
+  secondary_color?: string;
+  club?: Club | string;
+  league?: League | string;
+  primary_assets?: (Asset | string)[];
+  secondary_assets?: (Asset | string)[];
+  illustration?: Image;
+  resolved_primary_color?: string;
+  resolved_secondary_color?: string;
+  resolved_primary_assets?: (Asset | string)[];
+  resolved_secondary_assets?: (Asset | string)[];
+  resolved_illustration?: Image;
+}
