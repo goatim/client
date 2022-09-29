aec1af5e9b975202bf355fd66c2cc0cbd6e1f6c2
Lucien Perouze <lucien.perouze@cezemb.re>
2022-09-29 10:01:52 +0200

    Player asset search type

--- src/search/model.ts
+++ src/search/model.ts
@@ -1,6 +1,6 @@
 import Asset from '../trading/assets/model';
 
-export type SearchResultType = 'asset';
+export type SearchResultType = 'player-asset';
 
 export interface SearchResult {
   type: SearchResultType;
