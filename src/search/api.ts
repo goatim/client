889e8fcf7b3100d0f1f3bc321aef6050b69987c3
Lucien Perouze <lucien.perouze@cezemb.re>
2022-09-29 12:00:12 +0200

    Q query search

--- src/search/api.ts
+++ src/search/api.ts
@@ -3,18 +3,17 @@ import { useApi, ListRequestQuery } from '../api';
 import Search from './model';
 
 export interface SearchQuery extends ListRequestQuery {
-  q: string;
   types?: string;
 }
 
-export function useSearch(query: SearchQuery): UseQueryResult<Search> {
+export function useSearch(q?: string, query?: SearchQuery): UseQueryResult<Search> {
   const api = useApi();
   return useQuery<Search>(
     ['search', query],
     async () => {
-      const { data } = await api.get<Search>('/search', query);
+      const { data } = await api.get<Search>('/search', { ...query, q });
       return data;
     },
-    { enabled: !!query.query },
+    { enabled: !!q },
   );
 }
