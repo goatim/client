e79da3af4c078ee6e760582fe0a8809f27f06337
Lucien Perouze <lucien.perouze@cezemb.re>
2023-01-23 17:44:48 +0100

    Closed match notification

--- src/community/notifications/model.ts
+++ src/community/notifications/model.ts
@@ -3,6 +3,8 @@ import User from '../../auth/users/model';
 import Wallet from '../../market/wallets/model';
 import Asset from '../../trading/assets/model';
 import { OrderType } from '../../trading/orders/model';
+import Match from '../../soccer/matches/model';
+import Composition from '../../soccer/compositions/model';
 
 export interface NotificationOrderMatchPayload {
   type?: OrderType;
@@ -11,8 +13,14 @@ export interface NotificationOrderMatchPayload {
   asset?: Asset | string;
 }
 
+export interface NotificationClosedMatchPayload {
+  match?: Match | string;
+  composition?: Composition | string;
+}
+
 export interface NotificationEventMap {
   order_match: NotificationOrderMatchPayload;
+  closed_match: NotificationClosedMatchPayload;
 }
 
 export default interface Notification<
