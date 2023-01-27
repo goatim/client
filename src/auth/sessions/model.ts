4154018cb4d137ef53ccfab9bbaa1b2d4ec86e53
Lucien Perouze <lucien.perouze@cezemb.re>
2023-01-27 17:41:22 +0100

    Session user

--- src/auth/sessions/model.ts
+++ src/auth/sessions/model.ts
@@ -1,9 +1,10 @@
 import { Model } from '../../api';
+import User from '../users/model';
 
 export default interface Session extends Model<'session'> {
   expiration?: string;
   distribution?: string;
-  user?: string;
+  user?: User | string;
   method?: string;
   device_name?: string;
   bearer_token?: string;
