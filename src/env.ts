583e45f4366eba1588e8d1fbab9fc8c3ea83a493
Lucien Perouze <lucien.perouze@cezemb.re>
2023-04-25 18:14:25 +0200

    Safe env

--- src/env.ts
+++ src/env.ts
@@ -26,77 +26,77 @@ export interface Env {
 }
 
 function parseEnv(): Env {
-  const processEnv: NodeJS.ProcessEnv = process.env || {};
-  const windowEnv: NodeJS.ProcessEnv = window.env || {};
+  if (typeof process === 'undefined' || !process?.env) {
+    return {};
+  }
 
+  const processEnv: NodeJS.ProcessEnv = process.env;
   const env: Env = {};
 
-  if (windowEnv.SERVER_NAME !== undefined) {
-    env.SERVER_NAME = windowEnv.SERVER_NAME;
-  } else if (processEnv.REACT_APP_SERVER_NAME !== undefined) {
-    env.SERVER_NAME = processEnv.REACT_APP_SERVER_NAME;
+  if (processEnv.SERVER_NAME !== undefined) {
+    env.SERVER_NAME = processEnv.SERVER_NAME;
+  } else if (processEnv.NEXT_PUBLIC_SERVER_NAME !== undefined) {
+    env.SERVER_NAME = processEnv.NEXT_PUBLIC_SERVER_NAME;
   }
 
-  if (windowEnv.DOMAIN_NAME !== undefined) {
-    env.DOMAIN_NAME = windowEnv.DOMAIN_NAME;
-  } else if (processEnv.REACT_APP_DOMAIN_NAME !== undefined) {
-    env.DOMAIN_NAME = processEnv.REACT_APP_DOMAIN_NAME;
+  if (processEnv.DOMAIN_NAME !== undefined) {
+    env.DOMAIN_NAME = processEnv.DOMAIN_NAME;
+  } else if (processEnv.NEXT_PUBLIC_DOMAIN_NAME !== undefined) {
+    env.DOMAIN_NAME = processEnv.NEXT_PUBLIC_DOMAIN_NAME;
   }
 
-  if (windowEnv.ENV_NAME !== undefined) {
-    env.ENV_NAME = windowEnv.ENV_NAME;
-  } else if (processEnv.ENV_NAME !== undefined) {
+  if (processEnv.ENV_NAME !== undefined) {
     env.ENV_NAME = processEnv.ENV_NAME;
   } else if (processEnv.NODE_ENV !== undefined) {
     env.ENV_NAME = processEnv.NODE_ENV;
   }
 
-  if (windowEnv.RELEASE_VERSION !== undefined) {
-    env.RELEASE_VERSION = windowEnv.RELEASE_VERSION;
-  } else if (processEnv.REACT_APP_RELEASE_VERSION !== undefined) {
-    env.RELEASE_VERSION = processEnv.REACT_APP_RELEASE_VERSION;
+  if (processEnv.RELEASE_VERSION !== undefined) {
+    env.RELEASE_VERSION = processEnv.RELEASE_VERSION;
+  } else if (processEnv.NEXT_PUBLIC_RELEASE_VERSION !== undefined) {
+    env.RELEASE_VERSION = processEnv.NEXT_PUBLIC_RELEASE_VERSION;
   }
 
-  if (windowEnv.NODE_APP_INSTANCE !== undefined) {
-    env.NODE_APP_INSTANCE = parseInt(windowEnv.NODE_APP_INSTANCE, 10);
-  } else if (processEnv.REACT_APP_NODE_APP_INSTANCE !== undefined) {
-    env.NODE_APP_INSTANCE = parseInt(processEnv.REACT_APP_NODE_APP_INSTANCE, 10);
+  if (processEnv.NODE_APP_INSTANCE !== undefined) {
+    env.NODE_APP_INSTANCE = parseInt(processEnv.NODE_APP_INSTANCE, 10);
+  } else if (processEnv.NEXT_PUBLIC_NODE_APP_INSTANCE !== undefined) {
+    env.NODE_APP_INSTANCE = parseInt(processEnv.NEXT_PUBLIC_NODE_APP_INSTANCE, 10);
   }
 
-  if (windowEnv.DEFAULT_TIME_ZONE !== undefined) {
-    env.DEFAULT_TIME_ZONE = windowEnv.DEFAULT_TIME_ZONE;
-  } else if (processEnv.REACT_APP_DEFAULT_TIME_ZONE !== undefined) {
-    env.DEFAULT_TIME_ZONE = processEnv.REACT_APP_DEFAULT_TIME_ZONE;
+  if (processEnv.DEFAULT_TIME_ZONE !== undefined) {
+    env.DEFAULT_TIME_ZONE = processEnv.DEFAULT_TIME_ZONE;
+  } else if (processEnv.NEXT_PUBLIC_DEFAULT_TIME_ZONE !== undefined) {
+    env.DEFAULT_TIME_ZONE = processEnv.NEXT_PUBLIC_DEFAULT_TIME_ZONE;
   }
 
-  if (windowEnv.DEFAULT_LOCALE !== undefined) {
-    env.DEFAULT_LOCALE = windowEnv.DEFAULT_LOCALE;
-  } else if (processEnv.REACT_APP_DEFAULT_LOCALE !== undefined) {
-    env.DEFAULT_LOCALE = processEnv.REACT_APP_DEFAULT_LOCALE;
+  if (processEnv.DEFAULT_LOCALE !== undefined) {
+    env.DEFAULT_LOCALE = processEnv.DEFAULT_LOCALE;
+  } else if (processEnv.NEXT_PUBLIC_DEFAULT_LOCALE !== undefined) {
+    env.DEFAULT_LOCALE = processEnv.NEXT_PUBLIC_DEFAULT_LOCALE;
   }
 
-  if (windowEnv.DEFAULT_REGION_CODE !== undefined) {
-    env.DEFAULT_REGION_CODE = windowEnv.DEFAULT_REGION_CODE;
-  } else if (processEnv.REACT_APP_DEFAULT_REGION_CODE !== undefined) {
-    env.DEFAULT_REGION_CODE = processEnv.REACT_APP_DEFAULT_REGION_CODE;
+  if (processEnv.DEFAULT_REGION_CODE !== undefined) {
+    env.DEFAULT_REGION_CODE = processEnv.DEFAULT_REGION_CODE;
+  } else if (processEnv.NEXT_PUBLIC_DEFAULT_REGION_CODE !== undefined) {
+    env.DEFAULT_REGION_CODE = processEnv.NEXT_PUBLIC_DEFAULT_REGION_CODE;
   }
 
-  if (windowEnv.DEFAULT_CURRENCY !== undefined) {
-    env.DEFAULT_CURRENCY = windowEnv.DEFAULT_CURRENCY;
-  } else if (processEnv.REACT_APP_DEFAULT_CURRENCY !== undefined) {
-    env.DEFAULT_CURRENCY = processEnv.REACT_APP_DEFAULT_CURRENCY;
+  if (processEnv.DEFAULT_CURRENCY !== undefined) {
+    env.DEFAULT_CURRENCY = processEnv.DEFAULT_CURRENCY;
+  } else if (processEnv.NEXT_PUBLIC_DEFAULT_CURRENCY !== undefined) {
+    env.DEFAULT_CURRENCY = processEnv.NEXT_PUBLIC_DEFAULT_CURRENCY;
   }
 
-  if (windowEnv.API_HOST !== undefined) {
-    env.API_HOST = windowEnv.API_HOST;
-  } else if (processEnv.REACT_APP_API_HOST !== undefined) {
-    env.API_HOST = processEnv.REACT_APP_API_HOST;
+  if (processEnv.API_HOST !== undefined) {
+    env.API_HOST = processEnv.API_HOST;
+  } else if (processEnv.NEXT_PUBLIC_API_HOST !== undefined) {
+    env.API_HOST = processEnv.NEXT_PUBLIC_API_HOST;
   }
 
-  if (windowEnv.API_KEY !== undefined) {
-    env.API_KEY = windowEnv.API_KEY;
-  } else if (processEnv.REACT_APP_API_KEY !== undefined) {
-    env.API_KEY = processEnv.REACT_APP_API_KEY;
+  if (processEnv.API_KEY !== undefined) {
+    env.API_KEY = processEnv.API_KEY;
+  } else if (processEnv.NEXT_PUBLIC_API_KEY !== undefined) {
+    env.API_KEY = processEnv.NEXT_PUBLIC_API_KEY;
   }
 
   return env;
