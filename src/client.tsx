a870a9cc094b64b2dfa70228d0694709720e37f6
Lucien Perouze <lucien.perouze@cezemb.re>
2023-05-02 09:46:39 +0200

    Fix react query

--- src/client.tsx
+++ src/client.tsx
@@ -58,7 +58,7 @@ export function GoatimClient({
   return (
     <goatimClientContext.Provider value={value}>
       <ApiProvider config={apiConfig} cookie={cookie}>
-        <QueryClientProvider client={queryClient} contextSharing>
+        <QueryClientProvider client={queryClient}>
           <Hydrate state={state}>
             {children}
             <ReactQueryDevtools initialIsOpen={false} />
