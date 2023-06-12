354c342bfaebb368520becd8073c117173960e98
Lucien Perouze <lucien.perouze@cezemb.re>
2023-06-12 18:27:35 +0200

    Debug

--- src/api.tsx
+++ src/api.tsx
@@ -407,6 +407,7 @@ export function ApiProvider({
 
   const setConfig = useCallback(
     (_config: ApiConfig, duration?: DurationLike) => {
+      console.log('Set api config:', _config);
       setApiConfig(_config);
       if (persistConfig) {
         storeConfig(_config, duration);
@@ -417,6 +418,7 @@ export function ApiProvider({
 
   const setHost = useCallback(
     (host: string | null, duration?: DurationLike) => {
+      console.log('Set host:', host);
       setApiConfig((oldConfig) => ({ ...oldConfig, host: host || undefined }));
       if (persistConfig) {
         storeHost(host, duration);
@@ -427,6 +429,7 @@ export function ApiProvider({
 
   const setApiKey = useCallback(
     (apiKey: string | null, duration?: DurationLike) => {
+      console.log('Set api key:', apiKey);
       setApiConfig((oldConfig) => ({ ...oldConfig, api_key: apiKey || undefined }));
       if (persistConfig) {
         storeApiKey(apiKey, duration);
@@ -437,6 +440,7 @@ export function ApiProvider({
 
   const setLocale = useCallback(
     (locale: string | null, duration?: DurationLike) => {
+      console.log('Set locale:', locale);
       setApiConfig((oldConfig) => ({ ...oldConfig, locale: locale || undefined }));
       if (persistConfig) {
         storeLocale(locale, duration);
@@ -447,6 +451,7 @@ export function ApiProvider({
 
   const setBearerToken = useCallback(
     (bearerToken: string | null, duration?: DurationLike) => {
+      console.log('Set bearer token:', bearerToken);
       setApiConfig((oldConfig) => ({ ...oldConfig, bearer_token: bearerToken || undefined }));
       if (persistConfig) {
         storeBearerToken(bearerToken, duration);
