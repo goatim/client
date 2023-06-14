79b4b3ee774774d1e6082d1f54e64be675b87bbe
Lucien Perouze <lucien.perouze@cezemb.re>
2023-06-14 17:52:16 +0200

    User events

--- src/onboarding/userEvents.ts
+++ src/onboarding/userEvents.ts
@@ -1,3 +1,6 @@
 export enum OnboardingUserEvents {
   GENERAL_ONBOARDING_SEEN = 'general_onboarding_seen',
+  MATCHES_ONBOARDING_SEEN = 'matches_onboarding_seen',
+  MATCH_ONBOARDING_SEEN = 'match_onboarding_seen',
+  ASSET_ONBOARDING_SEEN = 'asset_onboarding_seen',
 }
